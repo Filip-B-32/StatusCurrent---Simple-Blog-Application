cb9dd2db6311b3c4edf1a01ff44a24e036c9ff62
Filip-B-32 <bfilip32@yahoo.com>
2024-07-24 11:03:45 +0300

    Update Backend: Tested via Postman Added custom responses

--- StatusCurrentAPI/StatusCurrentApp/views.py
+++ StatusCurrentAPI/StatusCurrentApp/views.py
@@ -9,7 +9,7 @@ class BlogPostListCreateView(mixins.ListModelMixin,
                              mixins.CreateModelMixin,
                              generics.GenericAPIView):
     
-    #handle listing and creating new blog post
+    # handle listing and creating new blog posts
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
@@ -17,22 +17,28 @@ class BlogPostListCreateView(mixins.ListModelMixin,
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        response = self.create(request, *args, **kwargs)
+        return Response({"message": "Blog post created successfully!", "data": response.data}, status=status.HTTP_201_CREATED)
 
 class BlogPostDetailView(mixins.RetrieveModelMixin,
                          mixins.UpdateModelMixin,
                          mixins.DestroyModelMixin,
                          generics.GenericAPIView):
     
-    #handle retrieving, updating, deleting specific blog post
+    # handle retrieving, updating, deleting specific blog posts
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        try:
+            return self.retrieve(request, *args, **kwargs)
+        except BlogPost.DoesNotExist:
+            return Response({"message": "Blog post not found."}, status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+        response = self.update(request, *args, **kwargs)
+        return Response({"message": "Blog post updated successfully", "data": response.data}, status=status.HTTP_200_OK)
 
     def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+        self.destroy(request, *args, **kwargs)
+        return Response({"message": "Blog post deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
